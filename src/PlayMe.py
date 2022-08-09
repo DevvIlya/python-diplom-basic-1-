cdd9e76668bea8d7bcb86e098b6b0a263e3c0bde
yi1lei6a3 <x.7machinegun@gmail.com>
2022-08-10 00:39:01 +0300

    add PlayMe

--- src/PlayMe.py
+++ src/PlayMe.py
@@ -4,13 +4,10 @@ import time
 from progress.bar import Bar
 import json
 from pprint import pprint
-from UploadPhoto import uploading_files_to_yandex_disk
-from UploadPhoto import creating_a_new_folder_on_yandex_disk
-from UploadPhoto import UploadPhoto
-from UploadPhoto import token_yandex
-from DownloadsPhoto import downloads_photo_from_vk
 from DownloadsPhoto import DownloadsPhoto
 from DownloadsPhoto import token_vk
+from UploadPhoto import UploadPhoto
+from UploadPhoto import token_yandex
 
 if __name__ == '__main__':
     user1 = UploadPhoto(token_yandex)
